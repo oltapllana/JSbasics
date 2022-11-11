322dd611d7c338f82acebb9e4825a05f393e8b27
oltap <oltap@LAPTOP-RAUC0V1F>
2022-11-11 23:55:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,99 @@
+// let numri  = prompt("Enter a numer");
+// let square = numri * numri;
+// console.log(square);
+
+// Execute a km to mile converter. 1 km = 0,621371 miles. State this as a constant in the code.
+// Execute a calculator. A user types in two numbers and the program puts out the results of + - * / actions with those numbers.
+// A user types in the values of a and b for a * x + b = 0 formula and the program counts the x value.
+// An employee’s monthly paycheck is $250 + 10% from sales. Request the sum of all monthly sales and count the salary.
+
+// let km = prompt('Enter km: ')*2;    
+// let converter = km*0.621371;
+
+// console.log(converter);
+//----------------------------------------------------------------------------
+
+// let number1 = prompt('Enter first number: ')*1;
+// let number2 = prompt('Enter second number: ')*1;
+
+// let sum = number1 + number2
+// console.log('Sum = ', sum);
+
+// let sub = number1 - number2;
+// console.log('Sub = ', sub);
+
+// let multiple = number1 * number2;
+// console.log(multiple);
+
+// let div = number1/number2;
+// console.log(div);
+//-------------------------------------------------------------------------
+
+// let a = prompt('enter a: ')*1;
+// let b = prompt('enter b: ')*1;
+// let x = -b/a;
+
+// console.log(x);
+
+//----------------------------------------------------------------------
+
+// let sales = prompt('enter sales')*12;
+// let salaryYear = 250*12;
+// let salary = salaryYear + 0.1*sales;
+// console.log(salary);
+
+// Request a user’s name and display the response “Hello, [name]”.
+// let name = prompt('Enter name: ');
+// console.log(`Hello ${name}`);
+
+
+// Request a user’s year of birth, count his age, and display the result. Store the current year as a constant.
+// const year = 2022;
+// let yearOfBirth = prompt('Enter your year of birth: ')*1;
+// console.log(year - yearOfBirth);
+
+
+// Request a length of a side of a square from a user and display the perimeter of the square.
+// let sideLength = prompt('Enter the side length of the square: ')*1;
+// let perimeter = sideLength*4;
+// console.log('perimetri: '+perimeter);
+
+
+// Request a radius of a circle and display the area of such a circle.
+// let radius = prompt('enter radius: ')*1;
+// const pi = 3.14;
+// let circleArea = pi*radius*radius;
+// console.log('Circle area: '+circleArea);
+
+
+// Request a distance in km between the two cities from a user, and the time they want to cover it within. Count the speed needed to be on time.
+// let distance = prompt('Enter distance between New York and Washington: in km')*1;
+// let time = prompt('Enter time you want to be there: ');
+// let speed = distance/time;
+// console.log(speed);
+
+
+// Create a currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a constant.
+// let dolar = prompt('Enter dollar: ')*1;
+// const exchange = 0.98;
+// let euro = dolar * exchange;
+// console.log(euro);
+
+// A user states a flash drive memory in GB. The program has to count how many 820MB files can be stored on this flash drive.
+// let flash = prompt('Flash drive memory in GB: ');
+// const filesGB = 0.82;
+// let filesStored = flash / filesGB;
+// console.log(filesStored);
+
+// A user types in an amount of money in a wallet and the price of one chocolate bar. The program counts how many chocolate bars the user can buy, and how much change will he/she have left.
+// let money = prompt('Money: ')*1;
+// let priceChoco = prompt('Price per choco bar: ')*1;
+// let bought = money / priceChoco;
+// let left = money - bought*priceChoco;
+// console.log(money + ' you can buy ' + bought + ' and money left ' + left );
+
+// The user types in a sum of bank deposits for 2 months with a yearly interest rate of 5%. The program counts the sum of interest.
+// let sum = prompt('Sum: ');
+// let interestperMonth = 0.05/12;
+// let interest = interestperMonth*2;
+// console.log(interest*sum);
